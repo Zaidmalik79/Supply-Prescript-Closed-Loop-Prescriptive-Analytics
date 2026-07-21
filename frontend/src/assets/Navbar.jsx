ab50b7862ad9a7e97e74066c39a7e83a2f62e1b5
Mohammad Zaid <Mohammadzaid67092@gmail.com>
2026-07-21 22:27:27 +0545

    Navbar

---
+++ frontend/src/assets/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav
+      style={{
+        background: "#0d6efd",
+        color: "white",
+        padding: "15px",
+        fontSize: "22px",
+        fontWeight: "bold",
+      }}
+    >
+      SupplyPrescript Dashboard
+    </nav>
+  );
+}
+
+export default Navbar;
